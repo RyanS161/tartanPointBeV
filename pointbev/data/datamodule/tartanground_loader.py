7e533b963a3a1a8c6accd439ac7c9256750a42d7
mloetscher <mloetscher@student.ethz.ch>
2025-03-23 23:29:46 +0100

    adapted batchstructure in dataload to contain data relevant for pointbev. WIP and untested

--- pointbev/data/datamodule/tartanground_loader.py
+++ pointbev/data/datamodule/tartanground_loader.py
@@ -11,6 +11,9 @@ import pytorch_lightning as pl
 import torch
 from tartanair import TartanAirDataset
 from torch.utils.data import random_split
+from scipy.spatial.transform import Rotation
+import json
+import os
 
 
 class TartangroundDatamodule(pl.LightningDataModule):
@@ -64,7 +67,61 @@ class TartangroundDatamodule(pl.LightningDataModule):
                                   camera_name=list(self.img_params.cams))
         train_len = int(0.8*len(data.dataset))
         self.train_data, self.val_data = random_split(data.dataset, [train_len, len(data.dataset)-train_len])
-        
+
+        # TODO could probably achieve that this is integrated into the data.dataset, but then need to adapt the 'create_image_dataset' func
+        # Load camera intrinsics from JSON files
+        self.camera_params = {}
+        env = self.envs[0]  # Assuming all cameras share same params across environments
+        difficulty = "Data_easy"  # hardcoded for now. Can parameterize in tartanground.yaml later
+        traj = "P0006"  # hardcoded for now. Can parameterize in tartanground.yaml later
+
+        for cam in self.img_params.cams: # iterates over lcam_front, back, left, right, top, bottom            
+            param_file = os.path.join(
+                self.dataroot, 
+                env, 
+                difficulty,
+                traj,
+                f"image_{cam}_pinhole",
+                f"camera_model_params_image_{cam}_pinhole.json"
+            )
+
+            pose_file = os.path.join(
+                self.dataroot, 
+                env, 
+                difficulty,
+                traj,
+                f"image_{cam}_pinhole.txt"
+            )
+
+            if os.path.exists(param_file):
+                with open(param_file, 'r') as f:
+                    content = json.load(f)
+                    self.camera_params[cam] = {
+                        'intrinsics': [
+                            [content['params']['fx'], 0, content['params']['cx']],
+                            [0, content['params']['fy'], content['params']['cy']],
+                            [0, 0, 1]
+                        ],
+                        'R_body_camera': content['R_raw_new']
+                    }
+            else:
+                print(f"Warning: Camera parameter file not found: {param_file}")
+                # Fall back to default params
+                self.camera_params[cam] = {
+                    'intrinsics': [[320.0, 0.0, 320.0], [0.0, 320.0, 320.0], [0.0, 0.0, 1.0]],
+                    'R_body_camera': [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]
+                }
+
+            if not os.path.exists(pose_file):
+                raise FileNotFoundError(f"Camera pose file not found: {pose_file}")
+            with open(pose_file, 'r') as f:
+                content = f.readlines()
+                self.camera_params[cam]['T_world_camera'] = [
+                    [float(x) for x in line.strip().split()] for line in content
+                ]
+
+
+            
 
     def train_dataloader(self):
         return torch.utils.data.DataLoader(
@@ -75,7 +132,8 @@ class TartangroundDatamodule(pl.LightningDataModule):
             drop_last=self.train_drop_last,
             # worker_init_fn=worker_rnd_init,
             pin_memory=self.pin_memory,
-            prefetch_factor=self.prefetch_factor
+            prefetch_factor=self.prefetch_factor,
+            collate_fn=self.collate_fn
         )
 
     def val_dataloader(self):
@@ -86,7 +144,8 @@ class TartangroundDatamodule(pl.LightningDataModule):
             drop_last=False,
             num_workers=self.num_workers,
             pin_memory=self.pin_memory,
-            prefetch_factor=self.prefetch_factor
+            prefetch_factor=self.prefetch_factor,
+            collate_fn=self.collate_fn
         )
 
     def test_dataloader(self):
@@ -101,6 +160,106 @@ class TartangroundDatamodule(pl.LightningDataModule):
             collate_fn=self.collate_fn,
         )
 
+    """
+    current batch sturcture:
+    {
+        'lcam_front': {
+            'image_0': np.array([H, W, 3]),
+            'image_1': np.array([H, W, 3]),
+            'motion': np.array([6]),
+        },
+        'lcam_back': {
+            'image_0': np.array([H, W, 3]),
+            'image_1': np.array([H, W, 3]),
+            'motion': np.array([6]),
+        },
+    
+    expected batch structure:
+    {
+    'imgs': {cam_name: tensor(B, 3, H, W)},   # camera → batch of images
+    'rots': tensor(B, N, 3, 3),
+    'trans': tensor(B, N, 3),
+    'intrins': tensor(B, N, 3, 3),
+    'bev_aug': tensor(B, 2, 3),
+    'egoTin_to_seq': tensor(B, 4, 4)
+    }  
+    """
+    def collate_fn(self, batch):
+        """
+        Convert list of samples → batch dict that matches PointBEV forward() inputs.
+        """
+        B = len(batch) # batch size
+        N = len(self.img_params.cams)  # number of cameras
+        imgs_list = []
+        intrins_list = []
+        trans_list = []
+        rots_list = []
+
+        for cam in self.img_params.cams:
+            cam_imgs = []
+            cam_trans = []
+            cam_rots = []
+
+            for sample in batch:
+                img = sample[cam]['image_0']
+                img_tensor = torch.from_numpy(img).permute(2, 0, 1).float() / 255.0
+                cam_imgs.append(img_tensor)
+
+                frame_idx = sample['frame_idx']
+                position = torch.tensor(self.camera_params[cam]['T_world_camera'][frame_idx][:3], dtype=torch.float32)
+                cam_trans.append(position)
+
+                # TODO function is assuming order [qx, q,y, qz, qw]. Verify that this is actually the case in the txt's
+                quat_rotation_world_camera = self.camera_params[cam]['T_world_camera'][frame_idx][3:7]
+                matrix_rotation_world_camera = Rotation.from_quat(quat_rotation_world_camera).as_matrix()
+                matrix_rotation_world_camera = torch.from_numpy(matrix_rotation_world_camera).float()
+                # TODO not sure if we need to transform frame from camera frame to body frame to world frame
+                R_body_camera = torch.tensor(self.camera_params[cam]['R_body_camera'], dtype=torch.float32) # converting R_body_camera to tensor and multiplying it
+                combined_rotation_matrix = torch.matmul(matrix_rotation_world_camera, R_body_camera)
+                cam_rots.append(combined_rotation_matrix)
+
+
+            imgs_list.append(torch.stack(cam_imgs))  # B x 3 x H x W
+
+            intrins_tensor = torch.tensor(self.camera_params[cam]['intrinsics'], 
+                                          dtype=torch.float32
+                                          )
+            # Repeat for batch size
+            intrins_list.append(intrins_tensor.unsqueeze(0).repeat(B, 1, 1))
+            trans_list.append(torch.stack(cam_trans))  # [B, 3]
+            rots_list.append(torch.stack(cam_rots))  # [B, 3, 3]
+
+        imgs = torch.stack(imgs_list, dim=0).permute(1, 0, 2, 3, 4).unsqueeze(1)  # B x T=1 x N x C=3 x H x W
+        intrins = torch.stack(intrins_list, dim=1)  # [B, N, 3, 3]
+        trans = torch.stack(trans_list, dim=1)  # [B, N, 3]
+        rots = torch.stack(rots_list, dim=1)  # [B, N, 3, 3]
+        
+
+        # Dummy camera intrinsics/extrinsics for now
+        # H, W = img_tensor.shape[1:]
+        # fx = fy = 320
+        # cx = cy = W // 2
+        # intrinsic_matrix = torch.tensor([[fx, 0, cx],
+        #                                 [0, fy, cy],
+        #                                   [0, 0, 1]], dtype=torch.float32)
+        # rots = torch.stack([torch.eye(3).clone() for _ in range(B * N)]).view(B, N, 3, 3)
+        # intrins = torch.stack([intrinsic_matrix.clone() for _ in range(B * N)]).view(B, N, 3, 3)
+        # rots = torch.stack([torch.eye(3).clone() for _ in range(B * N)]).view(B, N, 3, 3) # no rotation for now
+
+        # TODO rot with static and world camera pose
+        bev_aug = torch.stack([torch.eye(2, 3).clone() for _ in range(B)])  # (B, 2, 3) no aug for now
+        egoTin_to_seq = torch.stack([torch.eye(4).clone() for _ in range(B)]).unsqueeze(1)  # (B, T=1, 4, 4) static camera for now
+
+        return {
+            "imgs": imgs,
+            "rots": rots,
+            "trans": trans,
+            "intrins": intrins,
+            "bev_aug": bev_aug,
+            "egoTin_to_seq": egoTin_to_seq,
+        }
+
+    
     # def on_after_batch_transfer(self, batch, dataloader_idx):
     #     for key in ["binimg", "binimg_aug"]:
     #         if key in batch.keys():
